0905bf9114f6403220567ff79f7a382e7bc1306f
Jervin Macalawa <jervinmac>
2023-01-09 11:43:53 +0800

    done

--- components/head/report/ReportContainer.vue
+++ components/head/report/ReportContainer.vue
@@ -1,5 +1,77 @@
 <template>
   <div class="px-5 pt-5">
+    <v-dialog v-model="isView" width="" max-width="500">
+      <v-card class="pa-10">
+        <!-- <div>
+          {{selectedItem}}
+        </div> -->
+        <div>
+          <div align="center">
+            <v-img :src="selectedItem.image" height="200" width="200"></v-img>
+          </div>
+          <div>
+            <v-row>
+              <v-col cols="auto"> Firstname : </v-col>
+              <v-col>
+                {{ selectedItem.firstname }}
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="auto"> Lastname : </v-col>
+              <v-col>
+                {{ selectedItem.lastname }}
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="auto"> Year Graduated : </v-col>
+              <v-col>
+                {{ selectedItem.last_attended }}
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="auto"> Student Number : </v-col>
+              <v-col>
+                {{ selectedItem.student_number }}
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="auto"> Birthday : </v-col>
+              <v-col>
+                {{ selectedItem.birthdate }}
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="auto"> Address : </v-col>
+              <v-col>
+                {{ selectedItem.address }}
+              </v-col>
+            </v-row>
+            <div>
+              <div
+                v-for="x in work_data.filter(
+                  (data) => data.user_id == selectedItem.id
+                )"
+                :key="x"
+              >
+                <v-divider></v-divider>
+                <v-row>
+                  <v-col cols="12">
+                    <!-- <div align="end">
+                      <v-icon @click="editWork(x)"> mdi-pencil </v-icon>
+                      <v-icon @click="deleteWork(x)"> mdi-delete </v-icon>
+                    </div> -->
+                    <div>Company Name : {{ x.company_name }}</div>
+                    <div>Field : {{ x.field }}</div>
+                    <div>Position : {{ x.position }}</div>
+                    <div>Date Hired : {{ formatDate(x.date_joined) }}</div>
+                  </v-col>
+                </v-row>
+              </div>
+            </div>
+          </div>
+        </div>
+      </v-card>
+    </v-dialog>
     <v-row>
       <v-col>
         <v-row>
@@ -171,14 +243,9 @@
             </v-btn>
           </template>
           <v-list dense>
-            <v-list-item @click.stop="status(item, 'Accepted')">
+            <v-list-item @click.stop="view(item)">
               <v-list-item-content>
-                <v-list-item-title>Approve</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item @click.stop="status(item, 'Declined')">
-              <v-list-item-content>
-                <v-list-item-title>Decline</v-list-item-title>
+                <v-list-item-title>View Profile</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </v-list>
@@ -195,25 +262,33 @@ export default {
   computed: {
     ...mapState("users", ["users"]),
     ...mapState("report", ["report_data"]),
+    ...mapState("work", ["work_data"]),
   },
   created() {
+    this.$store.dispatch("work/view");
     this.$store.dispatch("report/view", {});
     this.$store.dispatch("users/view");
   },
   data() {
     return {
+      isView: false,
+      selectedItem: {},
       headers: [
         { text: "Student Number", value: "student_number" },
         { text: "Date Registered", value: "date_joined" },
         { text: "Firstname", value: "firstname" },
         { text: "Lastname", value: "lastname" },
         { text: "Email", value: "email" },
-
+        { text: "Actions", value: "opt" },
         ,
       ],
     };
   },
   methods: {
+    view(item) {
+      this.selectedItem = item;
+      this.isView = true;
+    },
     formatDate(item) {
       return moment(item).format("LL");
     },
